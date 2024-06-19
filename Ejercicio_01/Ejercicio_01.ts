9b4d9748319ecfc59cc5af109876457b114f57ee
rafael.aguilara <rafael.aguilara@sofftek.com>
2024-06-19 11:02:46 +0200

    first commit

---
+++ Ejercicio_01/Ejercicio_01.ts
@@ -0,0 +1,17 @@
+// https://www.typescriptlang.org/
+
+/* 
+coment
+in some 
+lines
+*/
+
+
+const constante: string = "constante";
+
+let stringType: string = "string";
+let numberType: number = 10
+let booleanType: boolean = true;
+
+console.log("hola mundo");
+
